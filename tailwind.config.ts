fc699a950ed9f99b5c9d3db7062c28a551f9f6d7
Drugvalue <gadbiohalal@gmail.com>
2026-01-04 17:34:31 -0800

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        muted: '#f3f4f6',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
+export default config;
